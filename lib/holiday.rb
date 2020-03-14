315684cd08e5509a721d6a2571b856ff09babd49
Cat Gomez <catherinedgomez@gmail.com>
2020-03-14 04:30:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,7 +24,10 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  holiday_hash[:winter][:christmas] << "food"
+  holiday_hash.each do |season, holiday_do|
+    if season == winter
+      holiday_do.each do |holiday, supply|
+        supply.update {"food"}
 end
 
 
